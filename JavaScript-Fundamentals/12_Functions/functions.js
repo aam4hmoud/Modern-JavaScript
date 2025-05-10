5c6332f7c361715e3b9de1ee4837768cbb10c882
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-05-10 14:40:38 +0300

    Add functions that need parameters

--- JavaScript-Fundamentals/12_Functions/functions.js
+++ JavaScript-Fundamentals/12_Functions/functions.js
@@ -4,3 +4,16 @@ function sayHello() {
 
 sayHello(); // Outputs: Hello, World!
 
+function greet(name, time) {
+    console.log("Good " + time + ", " + name + "!");
+}
+
+greet("Alice", "morning"); // Good morning, Alice!
+greet("Bob", "evening");   // Good evening, Bob!
+
+function greetWithDefault(name = "Guest") {
+    console.log("Welcome, " + name + "!");
+}
+
+greetWithDefault();        // Welcome, Guest!
+greetWithDefault("David"); // Welcome, David!
