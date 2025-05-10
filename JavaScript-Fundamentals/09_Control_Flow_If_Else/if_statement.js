922e37a05597763dcc3565909d515d7cda40c94e
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-05-10 14:03:39 +0300

    Add an example of nested if statement

--- JavaScript-Fundamentals/09_Control_Flow_If_Else/if_statement.js
+++ JavaScript-Fundamentals/09_Control_Flow_If_Else/if_statement.js
@@ -28,3 +28,17 @@ if (score >= 90) {
 } else {
     console.log("Needs improvement");
 }
+
+
+let num = 15;
+
+if (num > 0) {
+    console.log("Number is positive");
+    if (num % 2 === 0) {
+        console.log("Number is even");
+    } else {
+        console.log("Number is odd");
+    }
+} else {
+    console.log("Number is zero or negative");
+}
