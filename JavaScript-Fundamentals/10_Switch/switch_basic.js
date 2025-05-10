dea7b7ce7d3d151e6217c2f49645f657b27edb75
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-05-10 14:14:38 +0300

    Test grouping of cases

--- JavaScript-Fundamentals/10_Switch/switch_basic.js
+++ JavaScript-Fundamentals/10_Switch/switch_basic.js
@@ -50,3 +50,25 @@ switch (fruit) {
 
 console.log(response);
 // Outputs: How do you like them apples? Orange you glad I didn't say banana again?
+
+
+let grade = "B";
+let feedback;
+
+switch (grade) {
+    case "A":
+    case "B":
+        feedback = "Good job!";
+        break;
+    case "C":
+        feedback = "You can do better.";
+        break;
+    case "D":
+    case "F":
+        feedback = "You need to study more.";
+        break;
+    default:
+        feedback = "Invalid grade";
+}
+
+console.log(feedback); // Outputs: Good job!
