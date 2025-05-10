d0d5a7e6a0faa2b74c2af239219a29967284ab86
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-05-10 12:17:51 +0300

    Show that strict mode can be used inside functions

--- JavaScript-Fundamentals/03_Use_Strict/strict.js
+++ JavaScript-Fundamentals/03_Use_Strict/strict.js
@@ -2,3 +2,12 @@
 
 let x = 3.14;  // This will cause an error
 console.log(x);
+
+
+function strictFunction() {
+    'use strict';
+    let y = 3.14;  // This is fine
+    let z = 42;  // This will cause an error
+}
+
+strictFunction();
