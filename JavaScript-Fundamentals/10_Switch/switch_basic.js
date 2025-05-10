82ba66a07894a5ed3ce3c62c833833b91f9c0deb
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-05-10 14:13:31 +0300

    Test the effect of removing the break

--- JavaScript-Fundamentals/10_Switch/switch_basic.js
+++ JavaScript-Fundamentals/10_Switch/switch_basic.js
@@ -28,3 +28,25 @@ switch (day) {
 }
 
 console.log(dayName); // Outputs: Wednesday
+
+
+
+let fruit = "apple";
+let response;
+
+switch (fruit) {
+    case "banana":
+        response = "Banana is good!";
+        break;
+    case "apple":
+        response = "How do you like them apples?";
+        // No break here!
+    case "orange":
+        response += " Orange you glad I didn't say banana again?";
+        break;
+    default:
+        response = "I'm not familiar with that fruit.";
+}
+
+console.log(response);
+// Outputs: How do you like them apples? Orange you glad I didn't say banana again?
