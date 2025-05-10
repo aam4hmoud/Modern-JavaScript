8625516b153a2ee0cd7f6618c9370ea31e743555
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-05-10 14:01:39 +0300

    Add an if-else chain to the file

--- JavaScript-Fundamentals/09_Control_Flow_If_Else/if_statement.js
+++ JavaScript-Fundamentals/09_Control_Flow_If_Else/if_statement.js
@@ -5,3 +5,12 @@ if (temperature > 30) {
 }
 
 console.log("End of program");
+
+
+let hour = 14;
+
+if (hour < 12) {
+    console.log("Good morning!");
+} else {
+    console.log("Good afternoon!");
+}
